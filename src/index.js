7aa3a12bc0ad3bdd17fa9ec921c6a980d7eff9a0
Timofey Gribanov <timofey.gribanov@gmail.com>
2023-01-16 03:03:57 +0300

    feat:solving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reverseNum = '';
+
+    if (n < 0) {
+        n *= (-1);
+    };
+
+    while (n > 0) {
+        let remainder = n % 10;
+        reverseNum += remainder;
+        n = parseInt(n / 10);
+    }
+
+    return reverseNum;
 }
